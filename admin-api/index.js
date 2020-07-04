9fed2701065435a738a1c59e0c0b89542f677995
Fajar Dwi Darmanto <fajardwidarm@gmail.com>
2020-07-04 14:11:33 +0700

    update admin-api/index.js

--- admin-api/index.js
+++ admin-api/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 module.exports = (models) => {
   const router = express.Router();
   
+  router.use('/api/auth', require('./routes/auth')(models));
+  
   router.get('*', (req, res) => res.sendStatus(404));
   
   return router;
