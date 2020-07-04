c843aeb1fb29e79cd4b18d15b860c69d792fc694
Fajar Dwi Darmanto <fajardwidarm@gmail.com>
2020-07-04 14:08:32 +0700

    update models index and define models associations

--- models/index.js
+++ models/index.js
@@ -1,3 +1,19 @@
-module.exports = ctx => {
-  return {}
+module.exports = sequelize => {
+  const User = require('./user')(sequelize);
+  const Article = require('./article')(sequelize);
+  const Category = require('./category')(sequelize);
+  const Tag = require('./tag')(sequelize);
+  
+  User.hasMany(Article);
+  Article.belongsTo(User);
+  
+  Article.belongsToMany(Category, { through: 'PostCategories' });
+  Category.belongsToMany(Article, { through: 'PostCategories' });
+  
+  Article.belongsToMany(Tag, { through: 'PostTags' });
+  Tag.belongsToMany(Article, { through: 'PostTags' });
+  
+  sequelize.sync().catch(err => console.error(err));
+  
+  return { User, Article, Category, Tag }
 }
