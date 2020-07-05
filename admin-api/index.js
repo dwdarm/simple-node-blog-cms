0e984eeb57552489b303c386f329a65bc5b7b67f
Fajar Dwi Darmanto <fajardwidarm@gmail.com>
2020-07-05 14:43:47 +0700

    register users routes

--- admin-api/index.js
+++ admin-api/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 module.exports = (models) => {
   const router = express.Router();
+  const authentication = require('./middlewares/authentication')(models.User);
   
   router.use('/api/auth', require('./routes/auth')(models));
+  
+  router.use(authentication);
+  router.use('/api/users', require('./routes/user')(models));
   
   router.get('*', (req, res) => res.sendStatus(404));
   
