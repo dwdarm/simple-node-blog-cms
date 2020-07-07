cb76027aa4aedfe56dff27a8ca3ce7e529b67154
Fajar Dwi Darmanto <fajardwidarm@gmail.com>
2020-07-07 11:55:22 +0700

    register tag and category routes

--- admin-api/index.js
+++ admin-api/index.js
@@ -9,6 +9,8 @@ module.exports = (models) => {
   router.use(authentication);
   router.use('/api/users', require('./routes/user')(models));
   router.use('/api/articles', require('./routes/article')(models));
+  router.use('/api/categories', require('./routes/category')(models));
+  router.use('/api/tags', require('./routes/tag')(models));
   
   router.get('*', (req, res) => res.sendStatus(404));
   
