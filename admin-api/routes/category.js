6e6e89e7c2055d334e23d82521df59a6fc8521a1
Fajar Dwi Darmanto <fajardwidarm@gmail.com>
2020-07-07 11:52:50 +0700

    add category routes

---
+++ admin-api/routes/category.js
@@ -0,0 +1,140 @@
+const express = require('express');
+const userRole = require('../middlewares/user-role');
+
+module.exports = ({ Category }) => {
+  const router = express.Router();
+  
+  const getCategoryById = async (req, res, next) => {
+    try {
+      const category = await Category.findByPk(req.params.id);
+      if (!category) {
+        return res.status(404).send({
+          status: 'error',
+          message: 'Category not found'
+        });
+      }
+      
+      req.category = category;
+      next();
+    } catch(err) {
+      next(err);
+    }
+  }
+  
+  /**
+   * /GET /categories
+   */
+  
+  router.get('/', async (req, res, next) => {
+    try {
+      const categories = await Category.findAll();
+      res.send({ status: 'ok', data: categories });
+    } catch(err) {
+      next(err);
+    }
+  });
+  
+  /**
+   * /POST /categories
+   */
+  
+  router.post('/', userRole, async (req, res, next) => {
+    try {
+      if (!req.body.title || (typeof req.body.title !== 'string')) {
+        return res.status(400).send({
+          status: 'error',
+          message: 'Title not found'
+        });
+      }
+      
+      const isExist = await Category.findOne({ where: { title: req.body.title } });
+      if (isExist) {
+        return res.status(400).send({
+          status: 'error',
+          message: 'Category already exists'
+        });
+      }
+      
+      const category = await Category.create({ title: req.body.title });
+      res.status(201).send({ status: 'ok', data: category });
+      
+    } catch(err) {
+      next(err);
+    }
+  });
+  
+  /**
+   * /GET /categories/:id
+   */
+  
+  router.get('/:id', getCategoryById, async (req, res, next) => {
+    try {
+      res.send({ status: 'ok', data: req.category });
+    } catch(err) {
+      next(err);
+    }
+  });
+  
+  /**
+   * /PUT /categories/:id
+   */
+  
+  router.put('/:id', userRole, getCategoryById, async (req, res, next) => {
+    try {
+      if (typeof req.body.title === 'string') { 
+        const isExist = await Category.findOne({ where: { title: req.body.title } });
+        if (isExist) {
+          return res.status(400).send({
+            status: 'error',
+            message: 'Category already exists'
+          });
+        }
+        req.category.title = req.body.title; 
+      }
+      
+      await req.category.save();
+      await req.category.reload();
+      res.send({ status: 'ok', data: req.category });
+      
+    } catch(err) {
+      next(err);
+    }
+  });
+  
+  /**
+   * /DELETE /categories/:id
+   */
+  
+  router.delete('/:id', userRole, getCategoryById, async (req, res, next) => {
+    try {
+      await req.category.destroy();
+      res.send({ status: 'ok' });
+    } catch(err) {
+      next(err);
+    }
+  });
+  
+  /**
+   * /GET /categories/slug/:slug
+   */
+  
+  router.get('/slug/:slug', async (req, res, next) => {
+    try {
+      const category = await Category.findOne({ 
+        where: { slug: req.params.slug || '' }
+      });
+      if (!category) {
+        return res.status(404).send({
+          status: 'error',
+          message: 'Category not found'
+        });
+      }
+      
+      res.send({ status: 'ok', data: category });
+    } catch(err) {
+      next(err);
+    }
+  });
+  
+  return router;
+}
