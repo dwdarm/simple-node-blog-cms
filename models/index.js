91722fb5f195c3a048cefa36456bd3cf25959a7d
Fajar Dwi Darmanto <fajardwidarm@gmail.com>
2020-07-08 11:32:05 +0700

    add beforeCount global hook

--- models/index.js
+++ models/index.js
@@ -13,6 +13,29 @@ module.exports = sequelize => {
   Article.belongsToMany(Tag, { through: 'PostTags' });
   Tag.belongsToMany(Article, { through: 'PostTags' });
   
+  // solution from JamesMGreene (https://github.com/sequelize/sequelize/issues/9481)
+  sequelize.addHook('beforeCount', function (options) {
+    if (this._scope.include && this._scope.include.length > 0) {
+      options.distinct = true
+      options.col = this._scope.col || options.col || `"${this.options.name.singular}".id`
+    }
+
+    if (options.include && options.include.length > 0) {
+      let isInnerJoin = false;
+      options.include.forEach(e => {
+        if (typeof e === 'object') {
+          if (typeof e.where === 'object') {
+            isInnerJoin = true;
+          }
+        }
+      });
+      
+      if (!isInnerJoin) {
+        options.include = null;
+      }
+    }
+  })
+  
   sequelize.sync().catch(err => console.error(err));
   
   return { User, Article, Category, Tag }
