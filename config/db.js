0458890a8aaf6c047b586c59cb973f99b12b2611
Fajar Dwi Darmanto <fajardwidarm@gmail.com>
2020-07-13 12:19:37 +0700

    refactor config/db.js

--- config/db.js
+++ config/db.js
@@ -1,17 +1,20 @@
 const dbConfig = {}
 
-dbConfig.dialect = process.env.DATABASE_VENDOR || 'sqlite';
+if (!process.env.DATABASE_URL) {
+  dbConfig.dialect = process.env.DATABASE_VENDOR || 'sqlite';
 
-if (dbConfig.dialect === 'sqlite') {
-  dbConfig.storage = process.env.DATABASE_HOST || '.database.sqlite';
-} 
-else {
-  dbConfig.host = process.env.DATABASE_HOST;
-  dbConfig.database = process.env.DATABASE_DB;
-  dbConfig.username = process.env.DATABASE_USER;
-  dbConfig.database = process.env.DATABASE_DB;
+  if (dbConfig.dialect === 'sqlite') {
+    dbConfig.storage = process.env.DATABASE_HOST || '.database.sqlite';
+  } 
+  else {
+    dbConfig.host = process.env.DATABASE_HOST;
+    dbConfig.database = process.env.DATABASE_DB;
+    dbConfig.username = process.env.DATABASE_USER;
+    dbConfig.database = process.env.DATABASE_DB;
+  }
 }
 
-dbConfig.logging = process.env.NODE_ENV === 'development';
+dbConfig.logging = 
+  process.env.NODE_ENV === 'development' ? console.log : false ;
 
 module.exports = dbConfig;
