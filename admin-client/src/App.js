2f821b24f65b97c2d529420c8322a4a9312fda87
Fajar Dwi Darmanto <fajardwidarm@gmail.com>
2020-07-21 11:04:29 +0700

    update App component

--- admin-client/src/App.js
+++ admin-client/src/App.js
@@ -3,12 +3,14 @@ import { Provider } from 'react-redux';
 import configureStore from './store';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Auth from './Auth';
+import AuthRoute from './AuthRoute';
 import Index from './pages/Index';
 import Category from './pages/Category';
 import Tag from './pages/Tag';
 import NewArticle from './pages/NewArticle';
 import UpdateArticle from './pages/UpdateArticle';
 import Setting from './pages/Setting';
+import Login from './pages/Login';
 
 const store = configureStore();
 
@@ -20,12 +22,13 @@ export default class App extends React.Component {
         <Auth>
           <BrowserRouter>
             <Switch>
-              <Route path="/article-edit/:id"><UpdateArticle/></Route>
-              <Route path="/new"><NewArticle/></Route>
-              <Route path="/categories"><Category/></Route>
-              <Route path="/tags"><Tag/></Route>
-              <Route path="/setting"><Setting/></Route>
-              <Route path="/"><Index/></Route>
+              <AuthRoute path="/article-edit/:id"><UpdateArticle/></AuthRoute>
+              <AuthRoute path="/new"><NewArticle/></AuthRoute>
+              <AuthRoute path="/categories"><Category/></AuthRoute>
+              <AuthRoute path="/tags"><Tag/></AuthRoute>
+              <AuthRoute path="/setting"><Setting/></AuthRoute>
+              <AuthRoute path="/login"><Login/></AuthRoute>
+              <AuthRoute path="/"><Index/></AuthRoute>
             </Switch>
           </BrowserRouter>
         </Auth>
